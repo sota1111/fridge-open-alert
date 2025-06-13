284ab3c52e51572ac60f019d82143055b8a0f0c1
sota1111 <sota.moro@gmail.com>
2025-06-13 17:53:01 +0900

    hw_timerを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
+#include "peripheral/DistanceSensor.h"
+
+hw_timer_t * timer0 = NULL;
+DistanceSensor ds;
+void IRAM_ATTR onTimer() {
+  ds.update();
+}
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  ds.init();
+  // Configure hardware timer for 10ms interrupts
+  timer0 = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer0, &onTimer, true);
+  timerAlarmWrite(timer0, 10000, true);
+  timerAlarmEnable(timer0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
